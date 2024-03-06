b346a2939ad8aa9f6d68f465d90c72c900d771ff
Marija <marijap@mail.ru>
2024-03-06 15:56:58 +0000

    added code to the generateMarkdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,41 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
- 
+  return `
+  
+  # ${data.title}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests] (#tests)
+  - [License](#license)
+  - [Questions](#questions)
+0
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## License
+  This project is covered under ${data.license} license.
+
+  ## Questions
+  You can checkout my work in Github
+  ${data.username} (github.com/${data.username})
+  Should you have any questions you can reach me on the below email:
+  ${data.email}
+
 
 `;
 }
